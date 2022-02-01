e2d08b892889e7b0147d03df5c23d5b0e027493d
Felix Müller <felixrelleum@googlemail.com>
2022-02-01 15:35:15 +0100

    Deleted opendrive-ade-citygml4j-test folder

--- opendrive-ade-citygml4j-test/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module opendrive.ade.citygml4j.opendrive.ade.citygml4j.test.main {
-    requires org.citygml4j;
-    requires opendrive.ade.citygml4j.main;
-    uses org.citygml4j.xml.ade.ADEContext;
-    uses org.citygml.ade.opendrive.OpenDRIVEADEContext;
-}
