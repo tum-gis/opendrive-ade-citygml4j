357f1760462fc756053aca9e8f02a1ff79ad3037
Felix Müller <felixrelleum@googlemail.com>
2022-02-05 17:44:40 +0100

    Added OpenDRIVEConnectingRoad/Adapter classes

---
+++ src/main/java/org/citygml/ade/opendrive/adapter/road/OpenDRIVEConnectingRoadAdapter.java
@@ -0,0 +1,74 @@
+package org.citygml.ade.opendrive.adapter.road;
+
+import org.citygml.ade.opendrive.model.road.OpenDRIVEConnectingRoad;
+import org.citygml.ade.opendrive.model.road.OpenDRIVELaneSectionProperty;
+import org.citygml.ade.opendrive.model.road.OpenDRIVEStandardRoad;
+import org.citygml.ade.opendrive.model.road.e_trafficRule;
+import org.citygml.ade.opendrive.module.OpenDRIVEADEModule;
+import org.citygml4j.xml.adapter.transportation.AbstractTransportationSpaceAdapter;
+import org.gml.module.GML_LR_Module;
+import org.xmlobjects.annotation.XMLElement;
+import org.xmlobjects.builder.ObjectBuildException;
+import org.xmlobjects.gml.adapter.measures.LengthAdapter;
+import org.xmlobjects.serializer.ObjectSerializeException;
+import org.xmlobjects.stream.XMLReadException;
+import org.xmlobjects.stream.XMLReader;
+import org.xmlobjects.stream.XMLWriteException;
+import org.xmlobjects.stream.XMLWriter;
+import org.xmlobjects.xml.Attributes;
+import org.xmlobjects.xml.Element;
+import org.xmlobjects.xml.Namespaces;
+
+import javax.xml.namespace.QName;
+
+@XMLElement(name = "OpenDRIVEConnectingRoad", namespaceURI = OpenDRIVEADEModule.OPENDRIVEADE_NAMESPACE)
+public class OpenDRIVEConnectingRoadAdapter extends AbstractTransportationSpaceAdapter<OpenDRIVEConnectingRoad> {
+    @Override
+    public OpenDRIVEConnectingRoad createObject(QName name, Object parent) throws ObjectBuildException {
+        return new OpenDRIVEConnectingRoad();
+    }
+
+    @Override
+    public void buildChildObject(OpenDRIVEConnectingRoad object, QName name, Attributes attributes, XMLReader reader) throws ObjectBuildException, XMLReadException {
+        if (OpenDRIVEADEModule.OPENDRIVEADE_NAMESPACE.equals(name.getNamespaceURI())) {
+            switch (name.getLocalPart()) {
+                case "length":
+                    object.setOpenDRIVERoadLength(reader.getObjectUsingBuilder(LengthAdapter.class));
+                    break;
+                case "roadName":
+                    reader.getTextContent().ifPresent(object::setOpenDRIVERoadName);
+                    break;
+                case "rule":
+                    reader.getTextContent().ifPresent(v -> object.setOpenDRIVERoadTrafficRule(e_trafficRule.fromValue(v)));
+                    break;
+                case "laneSection":
+                    object.getOpenDRIVELaneSection().add(reader.getObjectUsingBuilder(OpenDRIVELaneSectionPropertyAdapter.class));
+                    break;
+            }
+        } else // If the namespace is not from the ADE then the element is from the citygml standard module
+            super.buildChildObject(object, name, attributes, reader);
+    }
+
+    @Override
+    public Element createElement(OpenDRIVEConnectingRoad object, Namespaces namespaces) throws ObjectSerializeException {
+        return Element.of(OpenDRIVEADEModule.OPENDRIVEADE_NAMESPACE, "OpenDRIVEConnectingRoad");
+    }
+
+    @Override
+    public void writeChildElements(OpenDRIVEConnectingRoad object, Namespaces namespaces, XMLWriter writer) throws ObjectSerializeException, XMLWriteException {
+        super.writeChildElements(object, namespaces, writer);
+
+        if (object.getOpenDRIVERoadLength() != null)
+            writer.writeElementUsingSerializer(Element.of(OpenDRIVEADEModule.OPENDRIVEADE_NAMESPACE, "length"), object.getOpenDRIVERoadLength(), LengthAdapter.class, namespaces);
+
+        if (object.getOpenDRIVERoadName() != null)
+            writer.writeElement(Element.of(OpenDRIVEADEModule.OPENDRIVEADE_NAMESPACE, "roadName").addTextContent(object.getOpenDRIVERoadName()));
+
+        if (object.getOpenDRIVERoadTrafficRule() != null)
+            writer.writeElement(Element.of(OpenDRIVEADEModule.OPENDRIVEADE_NAMESPACE, "rule").addTextContent(object.getOpenDRIVERoadTrafficRule().toValue()));
+
+        for (OpenDRIVELaneSectionProperty laneSectionProperty : object.getOpenDRIVELaneSection())
+            writer.writeElementUsingSerializer(Element.of(OpenDRIVEADEModule.OPENDRIVEADE_NAMESPACE, "laneSection"), laneSectionProperty, OpenDRIVELaneSectionPropertyAdapter.class, namespaces);
+
+    }
+}
--- src/main/java/org/citygml/ade/opendrive/model/road/OpenDRIVEConnectingRoad.java
+++ src/main/java/org/citygml/ade/opendrive/model/road/OpenDRIVEConnectingRoad.java
@@ -1,6 +1,60 @@
 package org.citygml.ade.opendrive.model.road;
 
+import org.citygml4j.model.ade.ADEObject;
+import org.citygml4j.model.common.TopLevelFeature;
 import org.citygml4j.model.transportation.AbstractTransportationSpace;
+import org.citygml4j.visitor.ObjectVisitor;
+import org.xmlobjects.gml.model.measures.Length;
 
-public abstract class OpenDRIVEConnectingRoad extends AbstractTransportationSpace {
+import java.util.ArrayList;
+import java.util.List;
+
+public class OpenDRIVEConnectingRoad extends AbstractTransportationSpace implements TopLevelFeature, OpenDRIVERoad, ADEObject {
+    private Length length;
+    private String roadName;
+    private e_trafficRule trafficRule;
+    private List<OpenDRIVELaneSectionProperty> laneSection;
+
+    @Override
+    public void setOpenDRIVERoadLength(Length length) {
+        this.length = length;
+    }
+
+    @Override
+    public Length getOpenDRIVERoadLength() {
+        return length;
+    }
+
+    @Override
+    public void setOpenDRIVERoadName(String roadName) {
+        this.roadName = roadName;
+    }
+
+    @Override
+    public String getOpenDRIVERoadName() {
+        return roadName;
+    }
+
+    @Override
+    public void setOpenDRIVERoadTrafficRule(e_trafficRule trafficRule) {
+        this.trafficRule = trafficRule;
+    }
+
+    @Override
+    public e_trafficRule getOpenDRIVERoadTrafficRule() {
+        return trafficRule;
+    }
+
+    @Override
+    public void setOpenDRIVELaneSection(List<OpenDRIVELaneSectionProperty> laneSection) {
+        this.laneSection = laneSection;
+    }
+
+    @Override
+    public List<OpenDRIVELaneSectionProperty> getOpenDRIVELaneSection() {
+        if (laneSection == null)
+            laneSection = new ArrayList<>();
+
+        return laneSection;
+    }
 }
