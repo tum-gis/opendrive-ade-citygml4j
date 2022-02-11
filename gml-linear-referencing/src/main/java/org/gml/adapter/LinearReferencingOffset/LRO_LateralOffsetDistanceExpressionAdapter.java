d6096a4b570e1c7d6597dc5e2642a48cf82bf863
Felix Müller <felixrelleum@googlemail.com>
2022-02-11 12:25:10 +0100

    Added package LinearReferencingOffset to GML3.3 module

---
+++ gml-linear-referencing/src/main/java/org/gml/adapter/LinearReferencingOffset/LRO_LateralOffsetDistanceExpressionAdapter.java
@@ -0,0 +1,7 @@
+package org.gml.adapter.LinearReferencingOffset;
+
+import org.gml.adapter.LinearReferencingSystem.LR_DistanceExpressionAdapter;
+import org.gml.model.LinearReferencingSystem.LR_DistanceExpression;
+
+public class LRO_LateralOffsetDistanceExpressionAdapter extends LR_DistanceExpressionAdapter {
+}
---
+++ gml-linear-referencing/src/main/java/org/gml/model/LinearReferencingOffset/LRO_LateralOffsetDistanceExpression.java
@@ -0,0 +1,6 @@
+package org.gml.model.LinearReferencingOffset;
+
+import org.gml.model.LinearReferencingSystem.LR_DistanceExpression;
+
+public class LRO_LateralOffsetDistanceExpression extends LR_DistanceExpression {
+}
